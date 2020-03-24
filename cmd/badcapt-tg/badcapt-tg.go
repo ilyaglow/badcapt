f24088d6bf36b492a3b86e8d6ee1dcc260013d08
Ilya Glotov <contact@ilyaglotov.com>
2020-03-25 02:01:29 +0300

    Add telegram output support

---
+++ cmd/badcapt-tg/badcapt-tg.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/ilyaglow/badcapt"
+)
+
+func main() {
+	listenIface := flag.String("i", "", "Interface name to listen")
+	chatID := flag.Int("chat", 0, "Telegram chat ID to report log into")
+	flag.Parse()
+
+	if *listenIface == "" {
+		panic(errors.New("no iface provided"))
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("API_KEY"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fn := func(ctx context.Context, rec *badcapt.Record) error {
+		j, err := json.Marshal(rec)
+		if err != nil {
+			return fmt.Errorf("json.Marshal: %w", err)
+		}
+
+		_, err = bot.Send(tgbotapi.NewMessage(int64(*chatID), string(j)))
+		if err != nil {
+			return fmt.Errorf("telegram send: %w", err)
+		}
+		return nil
+	}
+
+	bc, err := badcapt.New(badcapt.SetExportFunc(fn))
+	if err != nil {
+		panic(err)
+	}
+
+	log.Fatal(bc.Listen(*listenIface))
+}
--- go.mod
+++ go.mod
@@ -4,11 +4,13 @@ go 1.12
 
 require (
 	github.com/fortytw2/leaktest v1.2.0 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/google/go-cmp v0.2.0 // indirect
 	github.com/google/gopacket v1.1.17
 	github.com/mailru/easyjson v0.7.0 // indirect
 	github.com/olivere/elastic v6.2.26+incompatible
 	github.com/pkg/errors v0.8.1 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
 	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
 )
